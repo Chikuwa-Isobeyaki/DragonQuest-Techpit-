5b11b0e9711e856ed394658ad22b1976a8acc927
Chikuwa-Isobeyaki <kkisobe1213@icloud.com>
2022-04-19 12:00:27 +0900

    完成

---
+++ games_controller.rb
@@ -0,0 +1,56 @@
+require './message_dialog'
+class GamesController
+  include MessageDialog
+
+  EXP_CONSTANT = 2
+  GOLD_CONSTANT = 3
+
+  def battle(**params)
+
+    build_character(params)
+
+    loop do
+      @brave.attack(@monster)
+      break if battle_end?
+      @monster.attack(@brave)
+      break if battle_end?
+    end
+
+    battle_judgment
+
+  end
+
+
+  private
+
+  def build_character(**params)
+    @brave = params[:brave]
+    @monster = params[:monster]
+  end
+
+  def battle_end?
+    @brave.hp <= 0 || @monster.hp <= 0
+  end
+
+  def brave_win?
+    @brave.hp > 0
+  end
+
+  def battle_judgment
+    result = calculate_of_exp_and_gold
+
+      # end_messageを呼び出す
+      end_message(result)
+  end
+
+
+  def calculate_of_exp_and_gold
+    exp = (@monster.offense + @monster.defense) * EXP_CONSTANT
+    gold = (@monster.offense + @monster.defense) * GOLD_CONSTANT
+    result = {exp: exp, gold: gold}
+
+    result
+  end
+
+end
+
